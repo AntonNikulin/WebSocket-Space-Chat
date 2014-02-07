f4dc7da8fe5574cb165697fe3c0e5b6a0b247f1b
Anton Nikulin <dr.tallin@gmail.com>
2014-02-07 22:16:34 +0200

    moved script into separate file

---
+++ static/js/main.js
@@ -0,0 +1,15 @@
+
+
+    var canvas = document.querySelector("canvas");
+    var drawingSurface = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    var shipImage = new Image();
+    shipImage.addEventListener("load", imageLoadHandler, false);
+    shipImage.src="static/assets/sprites/Ship1.png";
+
+    function imageLoadHandler(){
+        drawingSurface.drawImage(shipImage, 0, 0);
+    }
