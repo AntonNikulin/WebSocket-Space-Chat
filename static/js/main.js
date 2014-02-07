134e568de41665219e1f5469e527bedd544b4f4c
Anton Nikulin <dr.tallin@gmail.com>
2014-02-07 22:44:58 +0200

    Ship object

--- static/js/main.js
+++ static/js/main.js
@@ -4,9 +4,14 @@ var drawingSurface = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+var ship =
+{
+    image: "static/assets/sprites/Ship1.png"
+}
+
 var shipImage = new Image();
 shipImage.addEventListener("load", imageLoadHandler, false);
-shipImage.src="static/assets/sprites/Ship1.png";
+shipImage.src=ship.image;
 
 function imageLoadHandler(){
     drawingSurface.drawImage(shipImage, 0, 0);
