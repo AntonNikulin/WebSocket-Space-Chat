78789fc5d50de90dabfbe4e75f1de6789f893faf
Anton Nikulin <dr.tallin@gmail.com>
2014-02-08 13:09:33 +0200

    canvas full screen

--- static/js/main.js
+++ static/js/main.js
@@ -1,12 +1,25 @@
 var canvas = document.querySelector("canvas");
 var drawingSurface = canvas.getContext("2d");
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+//full screen canvas
+canvas.width = document.body.clientWidth;
+canvas.height = document.body.clientHeight;
 
+//player's Ship object
 var ship =
 {
-    image: "static/assets/sprites/Ship1.png"
+    image: "static/assets/sprites/Ship1.png",
+    //position of source image in sprite
+    sourceX: 0,
+    sourceY: 0,
+    sourceWidth: 0,
+    sourceHeight: 0,
+
+    //position of the sprite on the canvas
+    x: 0,
+    y: 0,
+    width: 64,
+    height: 64
 }
 
 var shipImage = new Image();
