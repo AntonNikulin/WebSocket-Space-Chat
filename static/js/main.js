d5b86a67c528555eaf6c66f558cc18ef1a4a02d9
Anton Nikulin <dr.tallin@gmail.com>
2014-02-08 13:35:53 +0200

    + generalize: spites array, render method, update meth.

--- static/js/main.js
+++ static/js/main.js
@@ -1,31 +1,63 @@
 var canvas = document.querySelector("canvas");
 var drawingSurface = canvas.getContext("2d");
 
+//array to store the game sprites
+var sprites = [];
+
 //full screen canvas
 canvas.width = document.body.clientWidth;
 canvas.height = document.body.clientHeight;
 
-//player's Ship object
-var ship =
+//players Ship sprite object
+var shipObject =
 {
-    image: "static/assets/sprites/Ship1.png",
     //position of source image in sprite
     sourceX: 0,
     sourceY: 0,
-    sourceWidth: 0,
-    sourceHeight: 0,
+    sourceWidth: 52,
+    sourceHeight: 39,
 
     //position of the sprite on the canvas
     x: 0,
     y: 0,
-    width: 64,
-    height: 64
+    width: 52,
+    height: 39
 }
 
+//create sprite
+var playerShip = Object.create(shipObject);
+sprites.push(playerShip);
+
+//Load sprite's image
 var shipImage = new Image();
 shipImage.addEventListener("load", imageLoadHandler, false);
-shipImage.src=ship.image;
+shipImage.src= "static/assets/sprites/Ship1.png";
 
 function imageLoadHandler(){
-    drawingSurface.drawImage(shipImage, 0, 0);
+    //Update the sprite as soon as the image has been loaded
+    update();
+}
+
+function update() {
+    //Create animation loop
+    window.requestAnimationFrame(update, canvas);
+    //Render
+    render();
+}
+
+function render(){
+    //Clear previous frame
+    drawingSurface.clearRect(0, 0, canvas.width, canvas.height);
+
+    //Loop through all sprites
+    if (sprites.length !== 0){
+        for(var i=0; i<sprites.length; i++){
+            var sprite = sprites[i];
+            drawingSurface.drawImage(
+                shipImage,
+                sprite.sourceX, sprite.sourceY, sprite.sourceWidth, sprite.sourceHeight,
+                Math.floor(sprite.x), Math.floor(sprite.y), sprite.width, sprite.height
+            );
+        }
+    }
 }
