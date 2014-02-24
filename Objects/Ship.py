ea60d53e25c78bc4ac350f215e681c74cda4db9c
Anton Nikulin <dr.tallin@gmail.com>
2014-02-24 19:54:42 +0200

    + Ship.computeShipPosition()

--- Objects/Ship.py
+++ Objects/Ship.py
@@ -8,3 +8,6 @@ class Ship:
     def setPosition(self, x, y):
         self.__x = x
         self.__y = y
+
+    def computeShipPosition(self, vx, vy):
+        self.setPosition(self.__x + vx, self.__y + vy)
