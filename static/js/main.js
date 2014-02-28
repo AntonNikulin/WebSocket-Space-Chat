c02338c8b84a06388595ff1e9ea72a851e5c3ceb
Anton Nikulin <dr.tallin@gmail.com>
2014-02-28 23:11:35 +0200

    moved shipObject into sep file

--- static/js/main.js
+++ static/js/main.js
@@ -40,42 +40,8 @@ canvas.height = window.innerHeight;
 //array to store the game sprites
 var sprites = [];
 
-//players Ship sprite object
-var shipObject =
-{
-    //Movements
-    //rotation: degree
-    rotation: 0,
-
-    //velocity
-    vx: 0,
-    vy: 0,
-
-    //position of source image in sprite
-    sourceX: 0,
-    sourceY: 0,
-    sourceWidth: 35,
-    sourceHeight: 37,
-
-    //position of the sprite on the canvas
-    x: 0,
-    y: 0,
-    width: 35,
-    height: 37,
-
-    //text which displayed under the ship
-    text: "SHiP",
-
-    id: undefined,
-
-    setPosition: function(obj){
-        this.x = obj.x;
-        this.y = obj.y;
-    }
-}
-
 //create sprite
-var playerShip = Object.create(shipObject);
+var playerShip = Object.create(Blueprints.shipObject);
 multiplayer.getID();
 console.log("----ID: "+playerShip.id);
 sprites.push(playerShip);
---
+++ static/js/objectsBlueprints.js
@@ -0,0 +1,33 @@
+var Blueprints = {
+     shipObject:{
+        //Movements
+        //rotation: degree
+        rotation: 0,
+
+        //velocity
+        vx: 0,
+        vy: 0,
+
+        //position of source image in sprite
+        sourceX: 0,
+        sourceY: 0,
+        sourceWidth: 35,
+        sourceHeight: 37,
+
+        //position of the sprite on the canvas
+        x: 0,
+        y: 0,
+        width: 35,
+        height: 37,
+
+        //text which displayed under the ship
+        text: "SHiP",
+
+        id: undefined,
+
+        setPosition: function(obj){
+            this.x = obj.x;
+            this.y = obj.y;
+        }
+     }
+}
